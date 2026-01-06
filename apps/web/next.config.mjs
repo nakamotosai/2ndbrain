d282fd5bb00c8a51a81343368f8cbb788ea10d02
caisiyang <caisiyang@gmail.com>
2026-01-07 03:26:23 +0900

    Update next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,14 @@
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+};
 
 if (process.env.NODE_ENV === 'development') {
     await setupDevPlatform();
